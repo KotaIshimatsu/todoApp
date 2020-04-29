7fcb0c9a64a8d9ddb31d2a3b0a00bd20e98ca249
KotaIshimatsu <meerat0k.g@gmail.com>
2020-04-30 08:35:05 +0900

    コードを追加

--- TODOアプリ/js/script.js
+++ TODOアプリ/js/script.js
@@ -3,13 +3,14 @@ $(function() {
   $('#button1').click(function() {
     var textValue = $('#text1').val();
     $('ul').append(`<li class="list">${textValue}</li>`);
+    $('#text1').val('');
   });
   
   $('#button2').click(function() {
     $('.list:last').remove();
   });
 
-  $('ul').on('click', 'li', function() {
+  $('ul').on('click', 'list', function() {
     $(this).remove();
   });
 });
